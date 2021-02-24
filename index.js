66da8e84b4e793bfe546b8a40a46290551225dd2
Sanganu <kppkmsanu@gmail.com>
2021-02-24 06:26:25 -0700

    Base Class:

---
+++ index.js
@@ -0,0 +1,47 @@
+// ES6 Class Syntax
+class Calculator {
+     constructor(number1,number2){
+         this.number1 = number1
+         this.number2 = number2
+     }
+     //Methods to manipulate the properties
+
+     getterNumber1(){
+         return `The first numbers is ${this.number1}`
+     }
+  
+     getterNumber2(){
+         return `The second number is ${this.number2}`
+     }
+
+     setterNumber1(newValue){
+         this.number1 = newValue
+     }
+
+     setterNumber2(newValue){
+        this.number2 = newValue
+    }
+
+    addNumbers(){
+        return `The Addition of ${this.number1} and ${this.number2} is ${this.number1+this.number2}`
+    }
+
+    subNumbers(){
+        return `The Subtraction of ${this.number1} and ${this.number2} is ${this.number1-this.number2}`
+    }
+    multiplyNumbers(){
+        return `The Multiplication of ${this.number1} and ${this.number2} is ${this.number1*this.number2}`
+    }
+
+
+}
+
+
+
+// Create instance of class
+
+var firstOperation = new Calculator(120,130)
+// Accessing the class methods
+console.log(firstOperation.addNumbers());
+console.log(firstOperation.subNumbers());
+console.log(firstOperation.multiplyNumbers())
