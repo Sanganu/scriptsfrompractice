9da878ef55ce8be0dc63f852fe53a3fc00b757f9
Sanganu <kppkmsanu@gmail.com>
2021-03-01 17:49:28 -0700

    Methods in Child Class

--- app.js
+++ app.js
@@ -1,44 +1,62 @@
 // OOPS concept of Inheritance
-
-
+//Base Class
 class Base {
     constructor() {
         this.sum = 0
         this.product = 1
     }
-    get Addon() {
-        return this.sum
+    get addon() {
+        return this.sum //Getters for Summation
     }
-    get Product() {
-        return this.product
+    get mulitply() {
+        return this.product //Getter for Product
     }
     setSummation(number1) {
-        this.sum = this.sum + number1
+        this.sum = this.sum + number1 // Method
     }
-    set Multiplication(number1) {
-        this.product = this.product * number1
+    setMultiplication(number1) {
+        this.product = this.product * number1 
     }
 }
-
+// Inheritance
 class Series extends Base {
     constructor(array) {
-        super()
+        super() // Call Prarent Constructor
         this.numberSequence = array
+        this.average = 0
     }
     sumSeries() {
         for (let i = 0; i < this.numberSequence.length; i++) {
-            this.setSummation(this.numberSequence[i])
+            this.setSummation(this.numberSequence[i]) // Parent class method which is inherited
         }
+        console.log("Sum")
     }
-    get Series() {
+    get series() {
         return this.numberSequence
     }
-   
+    setProductsOf(){
+        for(let i=0;i < this.numberSequence.length; i++){
+            this.setMultiplication(this.numberSequence[i])
+        }
+    }
+    setAverage(){
+        this.sumSeries() // Calling method
+          this.average = (this.sum/this.numberSequence.length) // Using parent clas attributes
+     
+    }
+    get displayAverage(){
+        return this.average
+    }
 }
 
 let myNumbers = [12, 39, 54, 11, 90, 32]
 let Problem = new Series(myNumbers)
-console.log(Problem.Series)
-console.log(Problem.Product)
-Problem.sumSeries()
-console.log(Problem.Addon)
+console.log(`The Given Series: ${Problem.series}`) // Child Class getter
+Problem.setAverage()
+
+Problem.sumSeries() // Child Class 
+Problem.setProductsOf()
+console.log(`The Product of the Series: ${Problem.mulitply}`) //Parent Class Getter
+
+console.log(`The summation of the Series: ${Problem.addon}`) // Parent Class Getter
+console.log(`Average : ${Problem.displayAverage}`)
